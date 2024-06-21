11a4fac0d917f6630844803156b03b023c1a945d
Indhu0525 <indu01401@hmail.com>
2024-06-21 16:34:22 +0530

    deleted

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('transitionButton').addEventListener('click', function() {
-    var contents = document.querySelectorAll('.content');
-    contents.forEach(function(content) {
-        content.classList.toggle('transition');
-    });
-});
